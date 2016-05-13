892c49967406ce96dc1d148eb4b1d9875c4ab91b
John Wass <jwass3@gmail.com>
2016-05-13 00:38:39 -0400

    modeling of digital reading levels (ie. 0/1)

---
+++ src/main/scala/pigpio/scaladsl/Level.scala
@@ -0,0 +1,34 @@
+package pigpio.scaladsl
+
+
+trait Level {
+  def value: Int
+  def toBoolean: Boolean = value != 0
+}
+
+case object High extends Level {val value = 1}
+case object Low extends Level {val value = 0}
+
+object Level {
+  def apply(bool: Boolean) = if (bool) High else Low
+  def apply(int: Int) = int match {
+    case 0 => Low
+    case 1 => High
+    case _ => throw BadLevel()
+  }
+
+  object Implicits {
+    implicit def bool2level(b: Boolean): Level = Level(b)
+  }
+}
+
+object Levels {
+  val high = High
+  val on = high
+
+  val low = Low
+  val off = low
+
+  val set = on
+  val clear = off
+}
--- src/main/scala/pigpio/scaladsl/Results.scala
+++ src/main/scala/pigpio/scaladsl/Results.scala
@@ -3,3 +3,8 @@ package pigpio.scaladsl
 
 sealed trait GpioResult
 sealed trait GpioFailure extends RuntimeException with GpioResult
+
+/**
+ * [[Level]] results
+ */
+case class BadLevel() extends GpioFailure
