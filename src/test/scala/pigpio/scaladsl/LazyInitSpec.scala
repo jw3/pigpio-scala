6af0317b6471c833c0ddad45b6c527f76e56d213
John Wass <jwass3@gmail.com>
2016-05-12 22:01:00 -0400

    add spec to verify that the native library is lazily loaded

---
+++ src/test/scala/pigpio/scaladsl/LazyInitSpec.scala
@@ -0,0 +1,16 @@
+package pigpio.scaladsl
+
+import org.scalatest.{WordSpec, Matchers}
+
+class LazyInitSpec extends WordSpec with Matchers {
+  "library" should {
+    "load lazily" in {
+      PigpioLibrary.PI_OFF shouldBe 0
+    }
+    "load explicitly" in {
+      intercept[UnsatisfiedLinkError] {
+        PigpioLibrary.INSTANCE.gpioWaveTxStop
+      }
+    }
+  }
+}
