02589db932c69011c6067a5c3a5b7f348831fb6e
John Wass <jwass3@gmail.com>
2016-05-13 01:59:29 -0400

    additional models
    - PinMode
    - Pulls
    - More result types

--- src/main/scala/pigpio/scaladsl/Results.scala
+++ src/main/scala/pigpio/scaladsl/Results.scala
@@ -1,10 +1,43 @@
 package pigpio.scaladsl
 
+import pigpio.scaladsl.{PigpioLibrary => pigpio}
+
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success, Try}
 
 sealed trait GpioResult
 sealed trait GpioFailure extends RuntimeException with GpioResult
 
+sealed trait GpioOk extends GpioResult
+case class OK() extends GpioOk
+
+case class UnknownFailure() extends GpioFailure
+
 /**
- * [[Level]] results
+ * [[Gpio]] failures
  */
+sealed trait BadGpio extends GpioFailure
+case class BadUserGpio() extends BadGpio
+case class BadExGpio() extends BadGpio
+
+case class BadMode() extends GpioFailure
+case class BadPull() extends GpioFailure
 case class BadLevel() extends GpioFailure
+
+object GpioResult {
+  def apply(code: Int): GpioResult = code match {
+    case 0 => OK()
+    case pigpio.PI_BAD_USER_GPIO => throw BadUserGpio()
+    case pigpio.PI_BAD_GPIO => throw BadExGpio()
+    case pigpio.PI_BAD_MODE => throw BadMode()
+    case pigpio.PI_BAD_PUD => throw BadPull()
+    case _ => throw UnknownFailure()
+  }
+
+  def of(f: => Int): Try[GpioResult] = {
+    try Success(GpioResult(f))
+    catch {
+      case NonFatal(e) => Failure(e)
+    }
+  }
+}
